c23cc08c22c068c060c94271222c69cbf0de08d6
jamesho <8.james.ho@gmail.com>
2018-09-23 18:00:16 -0400

    add app drawer and map signout button

--- app/src/main/java/com/example/jamesho/dinero/MainActivity.java
+++ app/src/main/java/com/example/jamesho/dinero/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.jamesho.dinero;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,6 +21,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout mDrawerLayout;
 
     //TODO: put a recycler view in here that loads cardViews.
     //TODO: make a side panel where a user can access the sign out option
@@ -37,27 +42,33 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.signed_in_as);
         textView.setText("You are signed in as: " + accountName);
 
-        //Sign out button
-        // instantiate another google sign in object so that the user can sign out.
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // close drawer when item is tapped
+                mDrawerLayout.closeDrawers();
+                // sign out
+                signOut();
+                return true;
+            }
+        });
+    }
+
+    // A method that instantiates a googleSignIn Object to allow the user to sign out
+    private void signOut() {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
         final GoogleSignInClient mGoogleSignInClient;
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        findViewById(R.id.sign_out_button).setOnClickListener(new View.OnClickListener() {
+        mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
-            public void onClick(View view) {
-                mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        returnToSignIn();
-                    }
-                });
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
             }
         });
-    }
-
-    private void returnToSignIn() {
-        finish();
     }
 }
